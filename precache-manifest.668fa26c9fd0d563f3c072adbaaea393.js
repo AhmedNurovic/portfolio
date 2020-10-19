f20cc28d5e19b5761103a8c6ada78199aa2f6577
Ahmed Nurović <ahmed.nurovic@hotmail.com>
2020-10-20 00:16:24 +0200

    Updates

---
+++ precache-manifest.668fa26c9fd0d563f3c072adbaaea393.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb29ade5fa3fcbdb26091403eb68323e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85da5a53a5a49c712341",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "147fad6f47f69573d16d",
+    "url": "/portfolio/static/css/main.6c5a4838.chunk.css"
+  },
+  {
+    "revision": "85da5a53a5a49c712341",
+    "url": "/portfolio/static/js/2.6129d44b.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/portfolio/static/js/2.6129d44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147fad6f47f69573d16d",
+    "url": "/portfolio/static/js/main.0aff5fd9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d9cedab9004be6376c6e01dc1cb2c81c",
+    "url": "/portfolio/static/media/portfoliopic.d9cedab9.jpg"
+  }
+]);
